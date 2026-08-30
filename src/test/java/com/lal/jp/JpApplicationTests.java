a2a0d03e63dc9b33487a32be183cb3ad92a0723b
Abhishek Lal <abhisheklal1998@gmail.com>
2026-08-30 21:12:33 +0530

    Initial commit

---
+++ src/test/java/com/lal/jp/JpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lal.jp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
